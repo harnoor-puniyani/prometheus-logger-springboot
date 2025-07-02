deb509c787a98f11c32f62e5865edd15aa6e5846
harnoor <harnoor.puniyani@in.ey.com>
2025-07-02 07:37:54 +0530

    initial commit v2

---
+++ src/main/java/com/arzom/store/loggenerator/LogGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.arzom.store.loggenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LogGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arzom/store/loggenerator/LogGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arzom.store.loggenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
